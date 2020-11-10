0ed5d75787b8b55e0c5fb36970d4c1f7a19db868
jzolo22 <jzolotarev@gmail.com>
2020-11-10 17:05:35 -0500

    updates to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,26 @@ class SessionsController < ApplicationController
 
     end
 
+
+  def logout 
+    session.delete(:user_id)
+
+    redirect_to homepage_path
+  end 
+
+  def new 
+  end 
+
+  def create 
+    user = User.find_by(user_name: params[:session][:username])
+     
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+    #   redirect_to [we can decide where this should redirect]
+    else
+      flash[:error] = "Username or Password was incorrect"
+      redirect_to new_login_path
+    end 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root to: "sessions#index"
+  root to: "sessions#index", as: "homepage"
   resources :room_items
   resources :items
   resources :rooms
   resources :levels
   resources :users
+
+  get "/sessions/new", to: "sessions#new", as: "new_login"
+  delete "/sessions/logout", to: "sessions#destroy", as: 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
