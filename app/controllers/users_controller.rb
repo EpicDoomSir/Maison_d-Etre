d3474843f4cf37fafc4051c8db15e9e38af3d90e
EpicDoomSir <eitands@gmail.com>
2020-11-10 17:14:17 -0500

    added errors and login cookie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.create(params.require(:user).permit(:username, :password))
+        if @user.valid?
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:user_error] = @user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+
 end
