b5422844c5091b5838bd2406d3694ad2b757849c
jzolo22 <jzolotarev@gmail.com>
2020-11-10 22:06:44 -0500

    added rooms to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,18 +19,29 @@ Room.destroy_all
 Item.destroy_all
 RoomItem.destroy_all
 
+aviary = Room.create(name: "Aviary", artwork_path: "/app/assets/images/aviary/after_photo_tropical_rainforest_photo_by_jim_cunningham_photography.jpg")
+boiler = Room.create(name: "Boiler Room", artwork_path: , music_path: "")
+classroom = Room.create(name: "Classroom", artwork_path: , music_path: "")
+greenhouse = Room.create(name: "Greenhouse", artwork_path: , music_path: "")
+gymnasium = Room.create(name: "Gymnasium", artwork_path: , music_path: "")
+kitchen = Room.create(name: "Kitchen", artwork_path: , music_path: "")
+map = Room.create(name: "Map Room", artwork_path: , music_path: "")
+menagerie = Room.create(name: "Menagerie", artwork_path: , music_path: "")
+observatory = Room.create(name: "Observatory", artwork_path: , music_path: "")
 
-# room_ids & descriptions blank for now
-macaw = Item.create(name: "Macaw", image_path: "/app/assets/images/aviary/2.png", room_id: , description: )
-sharp_beak = Item.create(name: "Sharp Beak Bird", image_path: "/app/assets/images/aviary/3.png", room_id: , description: )
-cardinal = Item.create(name: "Cardinal", image_path: "/app/assets/images/aviary/4.png", room_id: , description: )
-owl = Item.create(name: "Owl", image_path: "/app/assets/images/aviary/5.png", room_id: , description: )
-toucan = Item.create(name: "Toucan", image_path: "/app/assets/images/aviary/6.png", room_id: , description: )
-ladybug = Item.create(name: "Ladybug Bird", image_path: "/app/assets/images/aviary/7.png", room_id: , description: )
-flamingo = Item.create(name: "Flamingo", image_path: "/app/assets/images/aviary/8.png", room_id: , description: )
-goldfinch = Item.create(name: "Goldfinch", image_path: "/app/assets/images/aviary/10.png", room_id: , description: )
-ibis = Item.create(name: "Ibis", image_path: "/app/assets/images/aviary/13.png", room_id: , description: )
-cockatoo = Item.create(name: "Cockatoo", image_path: "/app/assets/images/aviary/11.png", room_id: , description: )
+
+
+# descriptions blank for now
+macaw = Item.create(name: "Macaw", image_path: "/app/assets/images/aviary/2.png", room_id: aviary.id, description: "")
+sharp_beak = Item.create(name: "Sharp Beak Bird", image_path: "/app/assets/images/aviary/3.png", room_id: aviary.id, description: "")
+cardinal = Item.create(name: "Cardinal", image_path: "/app/assets/images/aviary/4.png", room_id: aviary.id, description: "")
+owl = Item.create(name: "Owl", image_path: "/app/assets/images/aviary/5.png", room_id: aviary.id, description: "")
+toucan = Item.create(name: "Toucan", image_path: "/app/assets/images/aviary/6.png", room_id: aviary.id, description: "")
+ladybug = Item.create(name: "Ladybug Bird", image_path: "/app/assets/images/aviary/7.png", room_id: aviary.id, description: "")
+flamingo = Item.create(name: "Flamingo", image_path: "/app/assets/images/aviary/8.png", room_id: aviary.id, description: "")
+goldfinch = Item.create(name: "Goldfinch", image_path: "/app/assets/images/aviary/10.png", room_id: aviary.id, description: "")
+ibis = Item.create(name: "Ibis", image_path: "/app/assets/images/aviary/13.png", room_id: aviary.id, description: "")
+cockatoo = Item.create(name: "Cockatoo", image_path: "/app/assets/images/aviary/11.png", room_id: aviary.id, description: "")
 
 
 
